c0ad746c42065f8760b44a936e9c1147a301119c
l30014168 <liuchang168@huawei.com>
2024-05-06 15:28:28 +0800

    优化crc8内存使用

--- Crc8.c
+++ Crc8.c
@@ -1,56 +1,133 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <threads.h>
+#include <time.h>
 
-uint8_t crc8(const uint8_t *data, const size_t length) {
-    uint8_t crc = 0;
-    uint8_t poly = 0x07;    // 多项式 x^8 + x^2 + x^1 + 1
-    for (size_t idx = 0; idx < length; ++idx) {
-        crc ^= data[idx];
-        for (uint8_t n = 0; n < 8; ++n) {
-            if (crc & 0x80) {
-                crc = (crc << 1) ^ poly;
+#define BUFFER_SIZE (1024 * 1024 * 16) // 16M buf
+
+uint8_t *buffer = NULL;
+int count = 0;
+bool producer_done = 0;
+
+mtx_t mutex;
+cnd_t can_produce;
+cnd_t can_consume;
+
+uint8_t crc = 0x00;
+
+#define CRC8_POLY 0x07
+#define CRC8_WIDTH 8
+
+static uint8_t crc8_table[1 << CRC8_WIDTH] = {
+    0x00, 0x07, 0x0e, 0x09, 0x1c, 0x1b, 0x12, 0x15, 0x38, 0x3f, 0x36, 0x31, 0x24, 0x23, 0x2a, 0x2d,
+    0x70, 0x77, 0x7e, 0x79, 0x6c, 0x6b, 0x62, 0x65, 0x48, 0x4f, 0x46, 0x41, 0x54, 0x53, 0x5a, 0x5d,
+    0xe0, 0xe7, 0xee, 0xe9, 0xfc, 0xfb, 0xf2, 0xf5, 0xd8, 0xdf, 0xd6, 0xd1, 0xc4, 0xc3, 0xca, 0xcd,
+    0x90, 0x97, 0x9e, 0x99, 0x8c, 0x8b, 0x82, 0x85, 0xa8, 0xaf, 0xa6, 0xa1, 0xb4, 0xb3, 0xba, 0xbd,
+    0xc7, 0xc0, 0xc9, 0xce, 0xdb, 0xdc, 0xd5, 0xd2, 0xff, 0xf8, 0xf1, 0xf6, 0xe3, 0xe4, 0xed, 0xea,
+    0xb7, 0xb0, 0xb9, 0xbe, 0xab, 0xac, 0xa5, 0xa2, 0x8f, 0x88, 0x81, 0x86, 0x93, 0x94, 0x9d, 0x9a,
+    0x27, 0x20, 0x29, 0x2e, 0x3b, 0x3c, 0x35, 0x32, 0x1f, 0x18, 0x11, 0x16, 0x03, 0x04, 0x0d, 0x0a,
+    0x57, 0x50, 0x59, 0x5e, 0x4b, 0x4c, 0x45, 0x42, 0x6f, 0x68, 0x61, 0x66, 0x73, 0x74, 0x7d, 0x7a,
+    0x89, 0x8e, 0x87, 0x80, 0x95, 0x92, 0x9b, 0x9c, 0xb1, 0xb6, 0xbf, 0xb8, 0xad, 0xaa, 0xa3, 0xa4,
+    0xf9, 0xfe, 0xf7, 0xf0, 0xe5, 0xe2, 0xeb, 0xec, 0xc1, 0xc6, 0xcf, 0xc8, 0xdd, 0xda, 0xd3, 0xd4,
+    0x69, 0x6e, 0x67, 0x60, 0x75, 0x72, 0x7b, 0x7c, 0x51, 0x56, 0x5f, 0x58, 0x4d, 0x4a, 0x43, 0x44,
+    0x19, 0x1e, 0x17, 0x10, 0x05, 0x02, 0x0b, 0x0c, 0x21, 0x26, 0x2f, 0x28, 0x3d, 0x3a, 0x33, 0x34,
+    0x4e, 0x49, 0x40, 0x47, 0x52, 0x55, 0x5c, 0x5b, 0x76, 0x71, 0x78, 0x7f, 0x6a, 0x6d, 0x64, 0x63,
+    0x3e, 0x39, 0x30, 0x37, 0x22, 0x25, 0x2c, 0x2b, 0x06, 0x01, 0x08, 0x0f, 0x1a, 0x1d, 0x14, 0x13,
+    0xae, 0xa9, 0xa0, 0xa7, 0xb2, 0xb5, 0xbc, 0xbb, 0x96, 0x91, 0x98, 0x9f, 0x8a, 0x8d, 0x84, 0x83,
+    0xde, 0xd9, 0xd0, 0xd7, 0xc2, 0xc5, 0xcc, 0xcb, 0xe6, 0xe1, 0xe8, 0xef, 0xfa, 0xfd, 0xf4, 0xf3,
+};
+
+void make_crc8_table(void) {
+    for (size_t idx = 0; idx < (1 << CRC8_WIDTH); idx++) {
+        uint8_t crc8 = idx;
+        for (uint8_t bit = 0; bit < CRC8_WIDTH; bit++) {
+            if ((crc8 & (1 << (CRC8_WIDTH - 1))) != 0) {
+                crc8 = (crc8 << 1) ^ CRC8_POLY;
             } else {
-                crc <<= 1;
+                crc8 <<= 1;
             }
         }
+        crc8_table[idx] = crc8;
+        printf("0x%02x, ", crc8_table[idx]);
+        if ((idx + 1) % 16 == 0) {
+            printf("\n");
+        }
     }
-    return crc;
+}
+
+void producer(void *arg) {
+    FILE *file = fopen(arg, "rb");
+    if (file == NULL) {
+        printf("Failed to open file\n");
+        return;
+    }
+
+    while (true) {
+        mtx_lock(&mutex);
+        while (count == BUFFER_SIZE) {
+            cnd_wait(&can_produce, &mutex);
+        }
+
+        count = fread(buffer, 1, BUFFER_SIZE, file);
+        if (count < BUFFER_SIZE) {
+            producer_done = true;
+        }
+        cnd_signal(&can_consume);
+        mtx_unlock(&mutex);
+
+        if (producer_done) {
+            break;
+        }
+    }
+
+    fclose(file);
+}
+
+void consumer(void *arg) {
+    while (1) {
+        mtx_lock(&mutex);
+        while (count == 0 && producer_done == false) {
+            cnd_wait(&can_consume, &mutex);
+        }
+
+        if (count == 0 && producer_done == true) {
+            break;
+        }
+
+
+        for (size_t idx = 0; idx < count; ++idx) {
+            crc = crc8_table[crc ^ buffer[idx]];
+        }
+        count = 0;
+        cnd_signal(&can_produce);
+        mtx_unlock(&mutex);
+    }
 }
 
 int main(int argc, char *argv[]) {
-    FILE *file = fopen(argv[1], "rb");
-    if (file == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
+    clock_t start = clock();
+    buffer = malloc(BUFFER_SIZE);
+    thrd_t producer_thread, consumer_thread;
+    mtx_init(&mutex, mtx_plain);
+    cnd_init(&can_produce);
+    cnd_init(&can_consume);
 
-    // 获取文件长度
-    fseek(file, 0, SEEK_END);
-    long file_length = ftell(file);
-    fseek(file, 0, SEEK_SET);
+    thrd_create(&producer_thread, (thrd_start_t) producer, argv[1]);
+    thrd_create(&consumer_thread, (thrd_start_t) consumer, NULL);
 
+    thrd_join(producer_thread, NULL);
+    thrd_join(consumer_thread, NULL);
 
-    // 分配足够的内存来存储文件内容
-    uint8_t *buffer = (uint8_t *)malloc(file_length);
-    if (buffer == NULL) {
-        perror("Memory allocation failed");
-        fclose(file);
-        return 1;
-    }
-
-    // 读取文件内容到缓冲区
-    if (fread(buffer, 1, file_length, file) != file_length) {
-        perror("Error reading file");
-        free(buffer);
-        fclose(file);
-        return 1;
-    }
-
-    uint8_t crc8_val = crc8(buffer, file_length);
-    printf("crc8: 0x%02x\n", crc8_val);
+    mtx_destroy(&mutex);
+    cnd_destroy(&can_produce);
+    cnd_destroy(&can_consume);
     free(buffer);
-    fclose(file);
+
+    printf("CRC8: 0x%02x\n", crc);
+    clock_t end = clock();
+    double duration = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("cost: %.3f s\n", duration);
     return 0;
 }
-
