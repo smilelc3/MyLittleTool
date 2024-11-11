6106b0a452c8e80f6916b828f17e1dc1bbfad610
l30014168 <liuchang168@huawei.com>
2024-11-11 15:55:41 +0800

    Hex2Ascii多行显示
    Change-Id: Ic449c5c484e075eb9ed920b19bd8b734c4f54cfc

--- Hex2Ascii.cpp
+++ Hex2Ascii.cpp
@@ -4,21 +4,22 @@
 #include <limits>
 #include <codecvt>
 #include <locale>
+#include <algorithm>
 
 #ifdef _WIN32
 #define NOMINMAX 1    // 禁用win自带的min max
+
 #include <Windows.h>
+
 #endif
 
-using namespace std;
-
 // 分割字符串，返回分割数组
-vector<string> StringSplit(const string &phrase, const string &delimiter) {
+std::vector<std::string> StringSplit(const std::string &phrase, const std::string &delimiter) {
     size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-    string token;
-    vector<string> res;
+    std::string token;
+    std::vector<std::string> res;
 
-    while ((pos_end = phrase.find(delimiter, pos_start)) != string::npos) {
+    while ((pos_end = phrase.find(delimiter, pos_start)) != std::string::npos) {
         token = phrase.substr(pos_start, pos_end - pos_start);
         pos_start = pos_end + delim_len;
         res.emplace_back(token);
@@ -34,7 +35,7 @@ static bool IsStringStartWith(const std::string &str, const std::string &prefix)
 }
 
 // 移除16进制前缀
-string RemoveHexPrefix(string &hex) {
+std::string RemoveHexPrefix(std::string &hex) {
     if (IsStringStartWith(hex, "0x") or IsStringStartWith(hex, "0X")) {
         hex = hex.substr(2);
     } else if (IsStringStartWith(hex, "x") or IsStringStartWith(hex, "X")) {
@@ -47,7 +48,7 @@ string RemoveHexPrefix(string &hex) {
 const wchar_t UNICODE_REPLACEMENT_CHAR = 0xFFFD;
 
 // 单个hex转ASCII
-wchar_t StringHex2Ascii(string &hex) {
+wchar_t StringHex2Ascii(std::string &hex) {
     // 可能的16进制表示形式: 0x12 AB ab 0xFa 0x1 6 0X12 x16 X0C
     hex = RemoveHexPrefix(hex);
     if (hex.empty() or hex.size() > 2) {
@@ -72,13 +73,24 @@ wchar_t StringHex2Ascii(string &hex) {
     return ascii;
 }
 
+std::string StrStrip(const std::string &str) {
+    // 使用 find_if 查找第一个非空白字符
+    auto start = std::find_if_not(str.begin(), str.end(), ::isspace);
+    // 使用 find_if 查找最后一个非空白字符
+    auto end = std::find_if_not(str.rbegin(), str.rend(), ::isspace).base();
+
+    // 返回去除两端空白的字符串
+    return std::move((start < end) ? std::string(start, end) : std::string());
+}
+
 
 // 函数入口
-string Hex2Ascii(const std::string &input) {
-    string delimiter = " ";     // 以空格为分隔符
+std::string Hex2Ascii(const std::string &input) {
+    std::string delimiter = " ";     // 以空格为分隔符
     auto split = StringSplit(input, delimiter);
-    wstring unicodeStr;
+    std::wstring unicodeStr;
     for (auto &&hex: split) {
+        hex = StrStrip(hex);
         if (not hex.empty()) {
             unicodeStr += StringHex2Ascii(hex);
         }
@@ -101,6 +113,7 @@ extern "C" {
     }
 }
 #else
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         return 0;
@@ -112,4 +125,5 @@ int main(int argc, char *argv[]) {
     std::cout << utf8Str << std::endl;
     return 0;
 }
+
 #endif
