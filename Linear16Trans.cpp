ae26cee68cfc8080befc8dd847db5f275ff805b5
smilelc3 <smile@liuchang.men>
2025-03-15 22:08:09 +0800

    Linear16ToDouble 支持 WASM

--- Linear16Trans.cpp
+++ Linear16Trans.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <sstream>
 #include <cstdint>
 
 // 位操作
@@ -27,13 +28,28 @@ double Linear16ToDouble(uint8_t exp, uint16_t man) {
     return ans;
 }
 
+// 用于 wasm
+#ifdef WASM_EMCC
+#include <emscripten.h>
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE
+    const char* C_Linear16Trans(const char *expStr, const char *manStr) {
+        auto exp = std::stoul(expStr, nullptr, 0);
+        auto man = std::stoul(manStr, nullptr, 0);
+        std::ostringstream output;
+        output << std::setprecision(4) << Linear16ToDouble(static_cast<uint8_t>(exp), static_cast<uint16_t>(man));
+        return strdup(output.str().c_str());
+    }
+}
+#else
 int main(int argc, char *argv[]) {
     if (argc == 3) {
         std::string linear16ExpStr = argv[1];
         std::string linear16ManStr = argv[2];
         auto exp = std::stoul(linear16ExpStr, nullptr, 0);
         auto val = std::stoul(linear16ManStr, nullptr, 0);
-        std::cout << std::setprecision(3) << Linear16ToDouble(exp, val) << std::endl;
+        std::cout << std::setprecision(4) << Linear16ToDouble(exp, val) << std::endl;
     }
     return 0;
 }
+#endif
